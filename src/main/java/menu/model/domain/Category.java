a6be8f5504fdc1392c0fcf7e8cad2db9417c42cf
yoondgu <doyou2ing@naver.com>
2022-12-17 14:55:56 +0900

    feat(model): 무작위 값으로 카테고리를 선택하는 기능 구현

--- src/main/java/menu/model/domain/Category.java
+++ src/main/java/menu/model/domain/Category.java
@@ -1,19 +1,36 @@
 package menu.model.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public enum Category {
-    JAPANESE(1, List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
-    KOREAN(2, List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
-    CHINESE(3, List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
-    ASIAN(4, List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
-    WESTERN(5, List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    JAPANESE("일식", 1, List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREAN("한식", 2, List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINESE("중식", 3, List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN("아시안", 4, List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    WESTERN("양식", 5, List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
 
+    private final String name;
     private final int randomValue;
     private final List<String> menus;
 
-    Category(int randomValue, List<String> menus) {
+    Category(String name, int randomValue, List<String> menus) {
+        this.name = name;
         this.randomValue = randomValue;
         this.menus = menus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Category takeRandomCategory(int pickValue) {
+        return Arrays.stream(values())
+                .filter(category -> Objects.equals(category.randomValue, pickValue))
+                .findFirst()
+                .orElseThrow(() -> {
+                    throw new IllegalArgumentException("카테고리에 지정되지 않은 난수입니다.");
+                });
     }
 }
