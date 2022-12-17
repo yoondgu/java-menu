01ec5fe62af727e3d36ee93c083b6def4b293212
yoondgu <doyou2ing@naver.com>
2022-12-17 14:47:37 +0900

    feat(model): 카테고리 별 메뉴 정보를 가지고 있는 기능 구현

---
+++ src/main/java/menu/model/domain/Category.java
@@ -0,0 +1,19 @@
+package menu.model.domain;
+
+import java.util.List;
+
+public enum Category {
+    JAPANESE(1, List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREAN(2, List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINESE(3, List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN(4, List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    WESTERN(5, List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+
+    private final int randomValue;
+    private final List<String> menus;
+
+    Category(int randomValue, List<String> menus) {
+        this.randomValue = randomValue;
+        this.menus = menus;
+    }
+}
