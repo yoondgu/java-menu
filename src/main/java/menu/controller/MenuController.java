c1db1b8b06bad089ed3686311e80c04c10066e92
yoondgu <doyou2ing@naver.com>
2022-12-21 16:57:28 +0900

    refactor(controller): 불필요한 멤버변수 삭제, 파라미터로 값 전달하도록 변경

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -12,28 +12,24 @@ import menu.view.OutputView;
 public class MenuController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private Coaches coaches;
 
     public void run() {
         outputView.printInformStart();
-        ExceptionHandler.retryForIllegalArgument(this::askCoachNames, outputView::printError);
-        coaches.getNames().forEach(this::saveDislikeMenusByCoach);
-        recommend();
+        Coaches coaches = ExceptionHandler.retryForIllegalArgument(Coaches::new, inputView::inputCoachNames,
+                outputView::printError);
+        coaches.getNames()
+                .forEach(coachNames -> saveDislikeMenusByCoach(coaches, coachNames));
+        recommend(coaches);
     }
 
-    private void askCoachNames() {
-        List<String> coachNames = inputView.inputCoachNames();
-        coaches = new Coaches(coachNames);
-    }
-
-    private void saveDislikeMenusByCoach(String coachName) {
+    private void saveDislikeMenusByCoach(Coaches coaches, String coachName) {
         DislikeMenus dislikeMenus = ExceptionHandler.retryForIllegalArgument(DislikeMenus::new,
                 () -> inputView.inputDisLikeMenu(coachName),
                 outputView::printError);
         coaches.updateDislikeMenusByCoach(coachName, dislikeMenus);
     }
 
-    private void recommend() {
+    private void recommend(Coaches coaches) {
         MenuRecommender menuRecommender = new MenuRecommender(coaches);
         List<String> dailyCategories = menuRecommender.getDailyCategories();
         Map<String, List<String>> coachMenus = menuRecommender.getCoachMenus();
