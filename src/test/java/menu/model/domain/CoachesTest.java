532d656b0286fa4ee623584e7d2e9e74b807e7e3
yoondgu <doyou2ing@naver.com>
2022-12-21 15:28:16 +0900

    test: 리팩토링으로 인한 Coaches 생성자 매소드 변경 테스트코드에 반영

--- src/test/java/menu/model/domain/CoachesTest.java
+++ src/test/java/menu/model/domain/CoachesTest.java
@@ -1,16 +1,20 @@
 package menu.model.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class CoachesTest {
-//    @Test
-//    void 코치목록정보_저장_중복이름_예외발생() {
-//        assertThatThrownBy(() -> new Coaches(List.of("포비", "포비")))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @Test
-//    void 코치목록정보_저장_인원수_예외발생() {
-//        assertThatThrownBy(() -> new Coaches(
-//                List.of("포비"), List.of(Collections.emptyList(), Collections.emptyList(), List.of("김치찌개"))))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
+    @Test
+    void 코치목록정보_저장_중복이름_예외발생() {
+        assertThatThrownBy(() -> new Coaches(List.of("포비", "포비")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 코치목록정보_저장_인원수_예외발생() {
+        assertThatThrownBy(() -> new Coaches(List.of("포비")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
